a93194217e568cb018e2b81ddae2fe325d268397
Prem Gangana <prem.gangana@amd.com>
2024-05-21 17:00:25 -0600

    further changes

--- compile-flags-per-source-file-plugin/src/main/java/CompileFlagsExtension.java
+++ compile-flags-per-source-file-plugin/src/main/java/CompileFlagsExtension.java
@@ -1,20 +1,36 @@
-import org.gradle.api.provider.Provider;
-import org.gradle.api.specs.Spec;
+package com.xilinx.infra.cpp.model;
 
 import java.io.File;
 
+import org.gradle.api.file.FileCollection;
+import org.gradle.api.provider.Provider;
+import org.gradle.api.specs.Spec;
+
 public interface CompileFlagsExtension {
     CompileFlags forSource(Spec<? super File> filterAction);
+    CompileFlags forSource(FileCollection cppSource);
+    CompileFlags forSourceUnder(File directory);
+    CompileFlags forSourceUnder(String directory);
 
     interface CompileFlags {
-        CompileFlags add(String item);
+        CompileFlags addOnLnx(String item);
 
-        CompileFlags add(Provider<? extends String> item);
+        CompileFlags addOnLnx(Provider<? extends String> item);
 
-        CompileFlags addAll(String... items);
+        CompileFlags addAllOnLnx(String... items);
 
-        CompileFlags addAll(Iterable<? extends String> items);
+        CompileFlags addAllOnLnx(Iterable<? extends String> items);
 
-        CompileFlags addAll(Provider<? extends Iterable<? extends String>> items);
+        CompileFlags addAllOnLnx(Provider<? extends Iterable<? extends String>> items);
+
+        CompileFlags addOnWin(String item);
+
+        CompileFlags addOnWin(Provider<? extends String> item);
+
+        CompileFlags addAllOnWin(String... items);
+
+        CompileFlags addAllOnWin(Iterable<? extends String> items);
+
+        CompileFlags addAllOnWin(Provider<? extends Iterable<? extends String>> items);
     }
 }
